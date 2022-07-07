0a6c199d82245df5d015e247325a17bacc6d4ff1
Dr. Maxim Orlovsky <orlovsky@pandoraprime.ch>
2022-07-07 21:30:06 +0200

    Update license header

--- src/peerd/opts.rs
+++ src/peerd/opts.rs
@@ -1,15 +1,14 @@
 // LNP Node: node running lightning network protocol and generalized lightning
 // channels.
 // Written in 2020-2022 by
-//     Dr. Maxim Orlovsky <orlovsky@pandoracore.com>
+//     Dr. Maxim Orlovsky <orlovsky@lnp-bp.org>
 //
 // To the extent possible under law, the author(s) have dedicated all
 // copyright and related and neighboring rights to this software to
 // the public domain worldwide. This software is distributed without
 // any warranty.
 //
-// You should have received a copy of the MIT License
-// along with this software.
+// You should have received a copy of the MIT License along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
 use std::net::{IpAddr, Ipv4Addr};
